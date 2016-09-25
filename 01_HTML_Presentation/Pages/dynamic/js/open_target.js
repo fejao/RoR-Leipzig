e3d784a23a27657901c6305796c2a6133c5b4cc6
Bruno Reboucas <kalle.knochen@gmail.com>
2016-09-25 05:29:59 +0200

    Finished Dynamic Web

--- 01_HTML_Presentation/Pages/dynamic/js/open_target.js
+++ 01_HTML_Presentation/Pages/dynamic/js/open_target.js
@@ -1,5 +1,6 @@
 $(function(){
   // $('.dropdown-menu a').click(function(e) {
+  // $('.active a').click(function(e) {
   $('.input a').click(function(e) {
       $('#content').hide().load( $(this).attr('href') , function(){
          $('#content').show()
